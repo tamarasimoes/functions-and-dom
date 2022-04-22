145a611de23a302f332f11a02b329e0a9bad5310
anaberger <ana.berger@betrybe.com>
2022-04-22 12:59:45 -0300

    add functions

---
+++ js/functions.js
@@ -0,0 +1,37 @@
+//Escreva uma função:
+// Essa função deverá receber 2 parametros.
+// a função deverá retornar a soma de 2 número.
+
+
+
+
+
+//Escreva uma função que retorne se a pessoa paga meia ou inteira no cinema.
+// a função deve receber um parametro, a idade da pessoa
+// a função deve fazer duas verificações, se a idade da pessoa for igual ou maior a 18 anos 
+//ela paga inteira 
+// se não ela pega meia
+// se não existir idade disparar um erro "insira uma idade"
+
+
+
+
+
+// Escreva uma função que receba um objeto e retorne o valor da propriedade cidade desse objeto
+// usar typeof para verificar se é um objeto
+
+
+
+
+//Adicionando propriedades em um objeto
+//Escreva uma função que receba uma chave e um valor e adicione ao nosso objeto abaixo
+
+
+
+
+//Escreva uma função que receba um array e um numero e retorne
+// o indice referente ao numero que foi passado
+
+
+
+
